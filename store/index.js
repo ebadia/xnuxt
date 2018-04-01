7f7fd4443d007a00c7ab746a103668b1b9e1ac05
ebadia <enric.badia@gmail.com>
2018-04-01 19:22:51 +0200

    fix: arranged store create

--- store/index.js
+++ store/index.js
@@ -2,7 +2,7 @@ import Vuex from 'vuex'
 import axios from 'axios'
 
 const createStore = () => {
-  return new Vuex:Store({
+  return new Vuex.Store({
     state: {},
     mutations: {},
     actions: {
