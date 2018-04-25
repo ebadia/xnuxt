934511f1b413fb2d8b28396f672eafd19eaf6608
ebadia <enric.badia@gmail.com>
2018-04-25 17:50:24 +0200

    fixed pages

--- components/XCity.vue
+++ components/XCity.vue
@@ -3,7 +3,7 @@
     <div class="no-gutters w-100 d-none d-md-block" style="height: auto;">
       <img :src="require(`~/assets/img/${imagen}`)" class="img-fluid" alt="xtremis">
     </div>
-    <div class="no-gutters w-100 d-md-none" style="height: auto;">
+    <div v-if="cta" class="no-gutters w-100 d-md-none" style="height: auto;">
       <img :src="require(`~/assets/img/${cta}`)" class="img-fluid w-100" alt="xtremis">
     </div>
   </div>
