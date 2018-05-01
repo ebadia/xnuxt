c27fd0e3156ca396c01baf2e39fe5ade9e583fae
ebadia <enric.badia@gmail.com>
2018-05-01 20:26:11 +0200

    with generate blog link with hid

--- pages/es/blog/id/index.vue
+++ pages/es/blog/id/index.vue
@@ -126,11 +126,11 @@
   head () {
     return {
       meta: [
-        { hid: 'og:url', property: 'og:url', content: `https://ebadia.github.io/xnuxt${this.$nuxt.$route.fullPath}/` },
-        { hid: 'og:type', property: 'og:type', content: 'article' },
-        { hid: 'og:title', property: 'og:title', content: `${this.$store.state.article.title.rendered}` },
-        { hid: 'og:description', property: 'og:description', content: `${this.$store.state.article.excerpt.rendered}` },
-        { hid: 'og:image', property: 'og:image', content: `${this.$store.state.featuredImage.source_url}` },
+        { hid: `og:url`, property: 'og:url', content: `https://ebadia.github.io/xnuxt${this.$nuxt.$route.fullPath}/` },
+        { hid: `og:type`, property: 'og:type', content: 'article' },
+        { hid: `og:title`, property: 'og:title', content: `${this.$store.state.article.title.rendered}` },
+        { hid: `og:description`, property: 'og:description', content: `${this.$store.state.article.excerpt.rendered}` },
+        { hid: `og:image`, property: 'og:image', content: `${this.$store.state.featuredImage.source_url}` },
       ]
     }
   }
